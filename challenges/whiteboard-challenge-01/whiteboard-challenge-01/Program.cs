882d1d8656f28fd00112bbc5df230af6b1eef5ec
ecaoile <unfie555@gmail.com>
2018-06-04 12:00:50 -0700

    added whiteboard in assets folder, working solution in challenges folder

---
+++ challenges/whiteboard-challenge-01/whiteboard-challenge-01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace whiteboard_challenge_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newArr = new int[5] { 1, 2, 3, 4, 5 };
+
+            Console.Write("Original array: ");
+            foreach (var num in newArr)
+                Console.Write($"{num} ");
+
+            int[] revArr = ArrayReverse(newArr);
+
+            Console.Write("\nReversed array: ");
+            foreach (var num in revArr)
+                Console.Write($"{num} ");
+
+            Console.WriteLine("\nThank you for playing. Press any button to exit.");
+            Console.ReadLine();
+        }
+
+        public static int[] ArrayReverse(int[] array)
+        {
+            int[] revArray = new int[array.Length];
+            for (int i = 0, j = array.Length - 1; i < array.Length; i++, j--)
+            {
+                revArray[i] = array[j];
+            }
+            return revArray;
+        }
+    }
+}
