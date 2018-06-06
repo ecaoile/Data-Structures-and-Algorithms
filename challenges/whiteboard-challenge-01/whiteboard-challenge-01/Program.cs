547a94a0ad626fc50eb70c4671013a4708a9b02e
ecaoile <unfie555@gmail.com>
2018-06-05 18:04:36 -0700

    adding comments to Program.cs, changed link to image to local relative directory

--- challenges/whiteboard-challenge-01/whiteboard-challenge-01/Program.cs
+++ challenges/whiteboard-challenge-01/whiteboard-challenge-01/Program.cs
@@ -6,22 +6,30 @@ namespace whiteboard_challenge_01
     {
         static void Main(string[] args)
         {
+            // initializing an array for demonstration purposes:
             int[] newArr = new int[5] { 1, 2, 3, 4, 5 };
 
+            // displaying the array to the console with a for loop
             Console.Write("Original array: ");
             foreach (var num in newArr)
                 Console.Write($"{num} ");
 
+            // invoking the function on the given array to create a new array
             int[] revArr = ArrayReverse(newArr);
 
+            // displaying the reversed array
             Console.Write("\nReversed array: ");
             foreach (var num in revArr)
                 Console.Write($"{num} ");
 
+            // closing message before user exits the app
             Console.WriteLine("\nThank you for playing. Press any button to exit.");
             Console.ReadLine();
         }
 
+        // reverse function: 
+        // input: takes in an array of integers
+        // output: array with values in reverse order
         public static int[] ArrayReverse(int[] array)
         {
             int[] revArray = new int[array.Length];
