65411c66fab54861d48f553e47d701612603908c
ecaoile <unfie555@gmail.com>
2018-06-05 23:46:45 -0700

    added commentary inside function

--- challenges/whiteboard-challenge-02/Whiteboard_Challenge_02/Whiteboard_Challenge_02/Program.cs
+++ challenges/whiteboard-challenge-02/Whiteboard_Challenge_02/Whiteboard_Challenge_02/Program.cs
@@ -33,23 +33,32 @@ namespace Whiteboard_Challenge_02
         public static int BinarySearch(int[] array, int key)
         {
             Console.WriteLine("\nRunning BinarySearch function...");
+            // set initial min value to 0 (first array element) and 
+            // max to array.Length - 1 (last array element)
             int currentSecMin = 0, currentSecMax = array.Length - 1;
             while (currentSecMin <= currentSecMax)
             {
+                // get a middle by adding the min and max and dividing by 2
                 int currentSecMid = (currentSecMin + currentSecMax) / 2;
+                
+                // handle an equal value found in the current section middle
                 if (key == array[currentSecMid])
                 {
                     Console.WriteLine($"Match with key of {key} find at index {currentSecMid}.");
                     return currentSecMid;
                 }
 
+                // if key is lower than the middle array element,
+                // move section max value to where the middle used to be
                 if (key < array[currentSecMid])
                     currentSecMax = currentSecMid - 1;
 
+                // else, move section min value to where middle used to be
                 else
                     currentSecMin = currentSecMid + 1;
             }
 
+            // return  -1 if there is no match after traversing through the array
             Console.WriteLine($"No match with key {key} found! Returning -1");
             return -1;
         }
