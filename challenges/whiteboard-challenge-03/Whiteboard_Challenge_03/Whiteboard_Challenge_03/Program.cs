7eb21f3934e2d36cf8616c488f532117daea2118
ecaoile <unfie555@gmail.com>
2018-06-07 00:51:03 -0700

    cleaned up code

--- challenges/whiteboard-challenge-03/Whiteboard_Challenge_03/Whiteboard_Challenge_03/Program.cs
+++ challenges/whiteboard-challenge-03/Whiteboard_Challenge_03/Whiteboard_Challenge_03/Program.cs
@@ -6,20 +6,26 @@ namespace Whiteboard_Challenge_03
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Whiteboard Challenge 03!");
+            Console.WriteLine("Welcome to Whiteboard Challenge 03!\n");
             // initialization of first test array
-            // expected return value: 132
-            int[,] firstArray2D = new int[,] { { 1, 2, 3}, { 4, 5, 6 }, { 7, 8 , 9 }, { 10, 11, 12 } };
+            // calling method with first test array as input
+            // expected return value: 210
+            int[,] firstArray2D = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 }, { 13, 14, 15 } };
+            Console.WriteLine("1st Array");
             AdjacentProduct(firstArray2D);
 
             // initialization of second test array
+            // calling method with second test array as input
             // expected return value: 64
             int[,] secondArray2D = new int[,] { { 1, 2, 1 }, { 2, 4, 2 }, { 3, 6, 8 }, { 7, 8, 1 } };
+            Console.WriteLine("2nd Array");
             AdjacentProduct(secondArray2D);
 
             // initialization of third test array
+            // calling method with third test array as input
             // expected return value: 2
             int[,] thirdArray2D = new int[,] { { 1 }, { 2 }};
+            Console.WriteLine("3rd Array");
             AdjacentProduct(thirdArray2D);
 
             Console.WriteLine("Thank you for playing! Press any button to exit.");
@@ -46,7 +52,8 @@ namespace Whiteboard_Challenge_03
                 }
                 Console.WriteLine();
             }
-                Console.WriteLine($"Length of 1st dimension: {datArray.GetLength(0)}");
+
+            Console.WriteLine($"\nLength of 1st dimension: {datArray.GetLength(0)}");
             Console.WriteLine($"Length of 2nd dimension: {datArray.GetLength(1)}");
             for (int i = 0; i < datArray.GetLength(0); i ++)
             {
@@ -55,8 +62,6 @@ namespace Whiteboard_Challenge_03
                     // check product of element and the item diagonal top left
                     if (i - 1 >= 0 && j - 1 >= 0)
                     {
-                        //Console.WriteLine("Hitting diagonal top left!");
-                       //Console.WriteLine($"Calculating the product of {datArray[i - 1, j - 1]} and {datArray[i, j]}.");
                         currentValue = datArray[i - 1, j - 1] * datArray[i, j];
                         if (currentValue > largestValue)
                             largestValue = currentValue;
@@ -65,8 +70,6 @@ namespace Whiteboard_Challenge_03
                     // check product of element and the item diagonal top right
                     if (i - 1 >= 0 && j + 1 < datArray.GetLength(1))
                     {
-                        //Console.WriteLine("Hitting diagonal top right!");
-                        //Console.WriteLine($"Calculating the product of {datArray[i - 1, j + 1]} and {datArray[i, j]}.");
                         currentValue = datArray[i - 1, j + 1] * datArray[i, j];
                         if (currentValue > largestValue)
                             largestValue = currentValue;
@@ -75,8 +78,6 @@ namespace Whiteboard_Challenge_03
                     // check product of element and the item diagonal bottom right
                     if (i + 1 < datArray.GetLength(0) && j + 1 < datArray.GetLength(1))
                     {
-                        //Console.WriteLine("Hitting diagonal bottom right!");
-                        //Console.WriteLine($"Calculating the product of {datArray[i + 1, j + 1]} and {datArray[i, j]}.");
                         currentValue = datArray[i + 1, j + 1] * datArray[i, j];
                         if (currentValue > largestValue)
                             largestValue = currentValue;
@@ -85,16 +86,14 @@ namespace Whiteboard_Challenge_03
                     // check product of element and the item diagonal bottom left
                     if (i - 1 >= 0 && j + 1 < datArray.GetLength(1))
                     {
-                        //Console.WriteLine("Hitting diagonal bottom left!");
-                        //Console.WriteLine($"Calculating the product of {datArray[i - 1, j + 1]} and {datArray[i, j]}.");
                         currentValue = datArray[i - 1, j + 1] * datArray[i, j];
                         if (currentValue > largestValue)
                             largestValue = currentValue;
                     }
+
                     // check product of element and the item directly above
                     if (i - 1 >= 0)
                     {
-                        //Console.WriteLine($"Calculating the product of {datArray[i - 1, j]} and {datArray[i, j]}.");
                         currentValue = datArray[i - 1, j] * datArray[i, j];
                         if (currentValue > largestValue)
                             largestValue = currentValue;
@@ -103,7 +102,6 @@ namespace Whiteboard_Challenge_03
                     // check product of element and the item directly below
                     if (i + 1 < datArray.GetLength(0))
                     {
-                        //Console.WriteLine($"Calculating the product of {datArray[i + 1, j]} and {datArray[i, j]}.");
                         currentValue = datArray[i + 1, j] * datArray[i, j];
                         if (currentValue > largestValue)
                             largestValue = currentValue;
@@ -112,7 +110,6 @@ namespace Whiteboard_Challenge_03
                     // check product of element and the item directly to the left
                     if (j - 1 >= 0)
                     {
-                        //Console.WriteLine($"Calculating the product of {datArray[i, j - 1]} and {datArray[i, j]}.");
                         currentValue = datArray[i, j - 1] * datArray[i, j];
                         if (currentValue > largestValue)
                             largestValue = currentValue;
@@ -121,13 +118,10 @@ namespace Whiteboard_Challenge_03
                     // check product of element and the item directly to the right
                     if (j + 1 < datArray.GetLength(1))
                     {
-                        //Console.WriteLine($"Calculating the product of {datArray[i, j + 1]} and {datArray[i, j]}.");
                         currentValue = datArray[i, j + 1] * datArray[i, j];
                         if (currentValue > largestValue)
                             largestValue = currentValue;
                     }
-                    //Console.WriteLine($"Value at index {i},{j}: {datArray[i, j]}");
-
                 }
             }
             Console.WriteLine($"The largest product is {largestValue}.\n");
