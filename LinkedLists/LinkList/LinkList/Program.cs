14b76dd5d07d003779bfd8d8929db4e4d3e84d34
ecaoile <unfie555@gmail.com>
2018-06-11 22:25:08 -0700

    completed readme and provided screenshot

--- LinkedLists/LinkList/LinkList/Program.cs
+++ LinkedLists/LinkList/LinkList/Program.cs
@@ -9,8 +9,9 @@ namespace LinkList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my humble singly linked list implementation!");
             TestLL();
+            Console.WriteLine("Thank you for watching. Press any button to exit.");
             Console.ReadLine();
         }
         /// <summary>
@@ -18,31 +19,32 @@ namespace LinkList
         /// </summary>
         static void TestLL()
         {
-            SinglyLinkedList datLL = new SinglyLinkedList(new Node(555));
-            datLL.Add(new Node(5));
-            datLL.Add(new Node(10));
-            
-            // We should see this print out in the console: 10 -> 5 -> 555
+            SinglyLinkedList datLL = new SinglyLinkedList(new Node(55));
+            datLL.Add(new Node(42));
+            datLL.Add(new Node(11));
+
+            // printing out starting value first
+            Console.WriteLine("\nHere are our starting values for reference:");
             datLL.Print();
             Console.WriteLine();
-            Console.WriteLine("\nLet's find node 5");
-            Node found = datLL.Find(5);
+            Console.WriteLine("\nLet's find node 42");
+            Node found = datLL.Find(42);
             Console.WriteLine(found.Value);
 
-            //let's add a node before 5
-            Console.WriteLine("\nLet's add a node before 5.");
-            datLL.AddBefore(new Node(12), new Node(5));
+            //let's add a node before 42
+            Console.WriteLine("\nLet's add a node before 42.");
+            datLL.AddBefore(new Node(24), new Node(42));
             datLL.Print();
             Console.WriteLine();
 
-            // let's add another node after 12
-            Console.WriteLine("\nLet's add a node after 12");
-            datLL.AddAfter(new Node(123), new Node(12));
+            // let's add another node after 24
+            Console.WriteLine("\nLet's add a node after 24");
+            datLL.AddAfter(new Node(35), new Node(24));
             datLL.Print();
             Console.WriteLine();
 
             // let's add another node to the end
-            Console.WriteLine("\nLet's add a node after 5");
+            Console.WriteLine("\nLet's add a node after 55");
             datLL.AddLast(new Node(345));
             datLL.Print();
             Console.WriteLine();
--- LinkedLists/README.md
+++ LinkedLists/README.md
@@ -1,8 +1,15 @@
 # CHALLENGE: Implement a Linked List 
-This program demonstrates a link list implementation from scratch.
+This program demonstrates a linked list implementation from scratch.
 
-## Visual:	
+## Data Structure:	
+A linked list is a group of objects that are connected to each other 
+(hence the term "linked"). The first node is known as the head. 
+Starting from the head, each node has its own value and a reference
+to the next node. THe last node has a reference to "null" because there
+is no next node.
 
+The linked list used in this program hold integers for values, but they can hold
+other data types as well.  
 
 ## Directions
 Implement a Singly linked list. Your LinkList should meet the following criteria:
@@ -14,8 +21,14 @@ Implement a Singly linked list. Your LinkList should meet the following criteria
     O(n) - .AddAfter(Node newNode, Node existingNode) - Adds a node after an existing node
     O(n) - .AddLast(Node node) - Adds a node to the end of a linked list. 
 
-## Solution
-![Challenge: LinkList image]("Challenge: LinkList")
+## Visual
+![Challenge: LinkList image](LinkList-SS.jpg "Challenge: LinkList")
+
+## Linked List Uses
+- Linked lists are useful when you don't know how many items will be in a list. Unlike arrays, linked lists can
+expand shrink as much as needed. 
+- A linked list's traversal term is also fairly predictable, so they are good for
+implementations where consistent time is necessary. A Big-O value of O(1) is possible when inserting at the head.
 
 
 
