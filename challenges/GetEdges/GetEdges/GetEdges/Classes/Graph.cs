2a5bb8c20d70a5fc4ee5c5ee14a385e6f337b039
ecaoile <unfie555@gmail.com>
2018-07-10 16:09:05 -0700

    working solutions

---
+++ challenges/GetEdges/GetEdges/GetEdges/Classes/Graph.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetEdges.Classes
+{
+    public class Graph
+    {
+        public List<Node> AllNodes { get; set; }
+
+        public Graph()
+        {
+            AllNodes = new List<Node>();
+        }
+
+        /// <summary>
+        /// 1. Adds a new vertice to the graph
+        /// </summary>
+        public void AddEdge(Node parent, Node child, decimal cost)
+        {
+            if (!parent.Children.Keys.Contains(child))
+            {
+                parent.Children.Add(child, cost);
+                child.Children.Add(parent, cost);
+            }
+
+            if (!AllNodes.Contains(parent))
+                AllNodes.Add(parent);
+
+            if (!AllNodes.Contains(child))
+                AllNodes.Add(child);
+        }
+
+        /// <summary>
+        /// 2. returns all of the nodes in the graph as a collection
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public List<Node> GetNodes(Node root)
+        {
+            return BreadthFirst(root);
+        }
+
+        /// <summary>
+        /// 3. returns a collection 
+        /// </summary>
+        /// <param name="root">root node to get neighbors from</param>
+        /// <returns>list of neighbor nodes to root</returns>
+        public List<Node> GetNeighbors(Node root)
+        {
+            var allNodes = GetNodes(root);
+
+            // initializing list with root's children
+            List<Node> demNeighbors = new List<Node>(root.Children.Keys);
+
+            // adding the "parents"
+            foreach (var item in allNodes)
+            {
+                if (item.Children.Keys.Contains(root) && !demNeighbors.Contains(item))
+                {
+                    demNeighbors.Add(item);
+                }
+            }
+
+            return demNeighbors;
+        }
+
+        /// <summary>
+        /// 4. returns the total number of nodes in the graph
+        /// </summary>
+        /// <param name="root">root node to start at</param>
+        /// <returns>integer counting nodes</returns>
+        public int Size(Node root)
+        {
+            return BreadthFirst(root).Count;
+        }
+
+        public List<Node> BreadthFirst(Node root)
+        {
+            List<Node> order = new List<Node>();
+            Queue<Node> breadth = new Queue<Node>();
+
+            root.Visited = true;
+            breadth.Enqueue(root);
+
+            while (breadth.TryPeek(out root))
+            {
+                Node front = breadth.Dequeue();
+                order.Add(front);
+
+                foreach (Node child in front.Children.Keys)
+                {
+                    if (!child.Visited)
+                    {
+                        child.Visited = true;
+                        breadth.Enqueue(child);
+                    }
+                }
+            }
+
+            foreach (var item in order)
+            {
+                item.Visited = false;
+            }
+
+            return order;
+        }
+
+
+        public decimal GetEdges(List<Node> nodes)
+        {
+            decimal total = 0;
+            if (nodes.Count == 1)
+                return total;
+
+            for(int i = 0; i < nodes.Count - 1; i++)
+            {
+                if (!GetNeighbors(nodes[i]).Contains(nodes[i+1]))
+                {
+                    Console.WriteLine("Flight does not connect!");
+                    return 0;
+                }
+
+                //List<decimal> allValues = nodes[i].Children.Keys
+                foreach (KeyValuePair<Node, decimal> pair in nodes[i].Children)
+                {
+                    if (pair.Key.Name == nodes[i+1].Name)
+                    {
+                        Console.WriteLine($"Adding value of {pair.Value} going from {nodes[i].Name} to {nodes[i+1].Name}");
+                        total += pair.Value;
+                    }
+                }
+            }
+            Console.WriteLine($"Your total is {total}");
+            return total;
+        }
+    }
+}
---
+++ challenges/GetEdges/GetEdges/GetEdges/Classes/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetEdges.Classes
+{
+    public class Node
+    {
+        public string Name { get; set; }
+
+        public Dictionary<Node, decimal> Children { get; set; }
+
+        public bool Visited { get; set; }
+
+        public Node(string value)
+        {
+            Name = value;
+            Children = new Dictionary<Node, decimal>();
+            Visited = false;
+        }
+    }
+}
