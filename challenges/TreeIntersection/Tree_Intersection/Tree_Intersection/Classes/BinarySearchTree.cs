25d2a6217944e1521d41b041888f98423a00781e
ecaoile <unfie555@gmail.com>
2018-07-17 23:57:14 -0700

    working solution

---
+++ challenges/TreeIntersection/Tree_Intersection/Tree_Intersection/Classes/BinarySearchTree.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree_Intersection.Classes
+{
+    public class BinarySearchTree
+    {
+        public Node Root { get; set; }
+
+        public BinarySearchTree(Node node)
+        {
+            Root = node;
+        }
+
+        /// <summary>
+        /// traverses a binary tree, printing out the values in "root, left, right" order
+        /// </summary>
+        /// <param name="node">the root node to start at</param>
+        public void PreOrder(Node node)
+        {
+            Console.WriteLine(node.Value);
+
+            if (node.LeftChild != null)
+            {
+                PreOrder(node.LeftChild);
+            }
+
+            if (node.RightChild != null)
+            {
+                PreOrder(node.RightChild);
+            }
+        }
+
+        /// <summary>
+        /// traverses a binary tree, printing out the values in "left, root, right" order
+        /// </summary>
+        /// <param name="node">the root node to start at</param>
+        public void InOrder(Node node)
+        {
+            if (node.LeftChild != null)
+            {
+                InOrder(node.LeftChild);
+            }
+
+            Console.WriteLine(node.Value);
+
+            if (node.RightChild != null)
+            {
+                InOrder(node.RightChild);
+            }
+        }
+
+        /// <summary>
+        /// traverses a binary tree, printing out the values in "left, right root" order
+        /// </summary>
+        /// <param name="node">the root node to start at</param>
+        public void PostOrder(Node node)
+        {
+            if (node.LeftChild != null)
+            {
+                PostOrder(node.LeftChild);
+            }
+
+            if (node.RightChild != null)
+            {
+                PostOrder(node.RightChild);
+            }
+
+            Console.WriteLine(node.Value);
+        }
+
+        /// <summary>
+        /// custom breadth first traversal that returns a list of int values
+        /// </summary>
+        /// <param name="node">the root node to start at</param>
+        public List<int> BreadthFirstList(Node root)
+        {
+            Queue<Node> breadth = new Queue<Node>();
+            List<int> demNums = new List<int>();
+            breadth.Enqueue(root);
+
+            while (breadth.TryPeek(out root))
+            {
+                Node front = breadth.Dequeue();
+                demNums.Add(front.Value);
+                //Console.WriteLine(front.Value);
+
+                if (front.LeftChild != null)
+                {
+                    breadth.Enqueue(front.LeftChild);
+                }
+
+                if (front.RightChild != null)
+                {
+                    breadth.Enqueue(front.RightChild);
+                }
+            }
+            return demNums;
+        }
+
+        /// <summary>
+        /// adds a node to the first vacant spot using breadth first traversal
+        /// </summary>
+        /// <param name="root">the root node to start at</param>
+        /// <param name="value">the value of the node to add</param>
+        /// <returns>boolean determining whether the add was successful</returns>
+        public bool Add(Node root, int value)
+        {
+            if (value == root.Value)
+                return false;
+
+            Node datNode = new Node(value);
+            Queue<Node> breadth = new Queue<Node>();
+            breadth.Enqueue(root);
+
+            while (breadth.TryPeek(out root))
+            {
+                Node front = breadth.Dequeue();
+                //Console.WriteLine(front.Value);
+
+                if (value == front.Value)
+                    return false;
+
+                if (value < front.Value)
+                {
+                    if (front.LeftChild == null)
+                    {
+                        front.LeftChild = datNode;
+                        //Console.WriteLine($"Added {value} to the left of {front.Value}.");
+                        return true;
+                    }
+                    else
+                    {
+                        breadth.Enqueue(front.LeftChild);
+                    }
+                }
+                
+                if (value > front.Value)
+                {
+                    if (front.RightChild == null)
+                    {
+                        front.RightChild = datNode;
+                        //Console.WriteLine($"Added {value} to the right of {front.Value}");
+                        return true;
+                    }
+                    else
+                    {
+                        breadth.Enqueue(front.RightChild);
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// searches for the value of a node in a tree
+        /// </summary>
+        /// <param name="root">the root node to start at</param>
+        /// <param name="value"></param>
+        /// <returns>the node if it was found</returns>
+        public Node Search(Node root, int value)
+        {
+            if (root != null)
+            {
+                Node datNode = new Node(value);
+                if (value == root.Value)
+                {
+                    Console.WriteLine($"Found a match with the value {root.Value}!");
+                    return root;
+                }
+
+                if (value < root.Value)
+                    return Search(root.LeftChild, value);
+
+                if (value > root.Value)
+                    return Search(root.RightChild, value);
+            }
+
+            Console.WriteLine("Sorry, but we could not find a match!");
+            return null;
+        }
+    }
+}
---
+++ challenges/TreeIntersection/Tree_Intersection/Tree_Intersection/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree_Intersection.Classes;
+
+namespace Tree_Intersection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Whiteboard Challenge 32!");
+            Node datBST1Root = new Node(150);
+            BinarySearchTree datBST1 = new BinarySearchTree(datBST1Root);
+            datBST1.Add(datBST1Root, 100);
+            datBST1.Add(datBST1Root, 250);
+            datBST1.Add(datBST1Root, 75);
+            datBST1.Add(datBST1Root, 160);
+            datBST1.Add(datBST1Root, 200);
+            datBST1.Add(datBST1Root, 175);
+            datBST1.Add(datBST1Root, 350);
+            datBST1.Add(datBST1Root, 125);
+            datBST1.Add(datBST1Root, 300);
+            datBST1.Add(datBST1Root, 500);
+
+            Node datBST2Root = new Node(42);
+            BinarySearchTree datBST2 = new BinarySearchTree(datBST2Root);
+            datBST2.Add(datBST2Root, 100);
+            datBST2.Add(datBST2Root, 600);
+            datBST2.Add(datBST2Root, 4);
+            datBST2.Add(datBST2Root, 15);
+            datBST2.Add(datBST2Root, 160);
+            datBST2.Add(datBST2Root, 125);
+            datBST2.Add(datBST2Root, 175);
+            datBST2.Add(datBST2Root, 200);
+            datBST2.Add(datBST2Root, 300);
+            datBST2.Add(datBST2Root, 350);
+            datBST2.Add(datBST2Root, 500);
+
+            TreeIntersection(datBST1, datBST2);
+            Console.WriteLine("\nThank you for watching! Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// takes two binary search trees and returns a list of common int values between the two
+        /// </summary>
+        /// <param name="datBST1">first BST</param>
+        /// <param name="datBST2">second BST</param>
+        /// <returns>a list of common int values</returns>
+        public static List<int> TreeIntersection(BinarySearchTree datBST1, BinarySearchTree datBST2)
+        {
+            List<int> demNums1 = datBST1.BreadthFirstList(datBST1.Root);
+            List<int> treeIntersection = new List<int>();
+
+            foreach (int num in demNums1)
+            {
+                var result = datBST2.Search(datBST2.Root, num);
+                if (result != null)
+                    treeIntersection.Add(result.Value);
+            }
+
+            Console.WriteLine("Here's your list of common numbers:");
+            Console.Write("[ ");
+            foreach (int num in treeIntersection)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.Write("]");
+
+            return demNums1;
+        }
+    }
+}
