f7b5ac0e7243bf71ca51e377d1e0f19662a59033
ecaoile <unfie555@gmail.com>
2018-07-30 21:53:51 -0700

    working solution and tests

---
+++ challenges/MergeSort/Merge_Sort/XUnitMergeSort/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using static Merge_Sort.Program;
+namespace XUnitMergeSort
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData((new int[] { 3, 5, 1, 8, 7 }), (new int[] { 1, 3, 5, 7, 8 }))]
+        [InlineData((new int[] { 4, 5, 0, -1, 3 }), (new int[] { -1, 0, 3, 4, 5 }))]
+        [InlineData((new int[] { 1, -1, 0 }), (new int[] { -1, 0, 1}))]
+        public void CanArrangeUnsortedArray(int[] input, int[] output)
+        {
+            Assert.Equal(output, MergeSort(input, 0, input.Length - 1));
+        }
+
+        [Theory]
+        [InlineData((new int[] { 0 }), (new int[] { 0 }))]
+        [InlineData((new int[] { -1, 0, 1 }), (new int[] { -1, 0, 1 }))]
+        [InlineData((new int[] { -10, 2, 3, 7 }), (new int[] { -10, 2, 3, 7 }))]
+        public void CanMaintainAlreadySortedArray(int[] input, int[] output)
+        {
+            Assert.Equal(output, MergeSort(input, 0, input.Length - 1));
+        }
+    }
+}
